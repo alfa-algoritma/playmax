1c1a78e2ab520840bfc26bdc425b7b80efd0e8f4
Muhammad Alfa <alphambrk@gmail.com>
2025-09-30 23:53:11 +0700

    feat: Menyelesaikan Tugas 5 PBP

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,34 @@
-from django.forms import ModelForm
+from django import forms
 from main.models import Product
 
-class ProductForm(ModelForm):
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description", "category", "thumbnail", "stock"]
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500',
+                'placeholder': 'Masukkan nama produk'
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500',
+                'placeholder': 'Masukkan harga'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500',
+                'rows': 4,
+                'placeholder': 'Masukkan deskripsi produk'
+            }),
+            'category': forms.TextInput(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500',
+                'placeholder': 'Contoh: Shoes, Apparel'
+            }),
+            'thumbnail': forms.URLInput(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500',
+                'placeholder': 'Masukkan URL gambar'
+            }),
+            'stock': forms.NumberInput(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500',
+                'placeholder': 'Masukkan jumlah stok'
+            }),
+        }
